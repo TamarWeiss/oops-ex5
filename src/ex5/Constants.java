862bbdccc3a0a65c195f74fa1ccd8741358a9fca
shrafers <hillel.lewin@gmail.com>
2025-01-29 14:30:43 +0200

    added docs

--- src/ex5/Constants.java
+++ src/ex5/Constants.java
@@ -4,15 +4,25 @@ import ex5.parser.Types;
 
 import java.util.Arrays;
 
+/** Constants used throughout the s-Java verification process */
 public class Constants {
+    /** final keyword */
     public static final String FINAL = "final";
+    /** void keyword */
     public static final String VOID = "void";
+    /** Identifier pattern */
     public static final String WHITESPACE = "\\s+";
+    /** Identifier pattern */
     public static final String COMMA = "\\s*,\\s*";
+    /** Identifier pattern */
     public static final String EQUALS = "\\s*=\\s*";
+    /** Identifier pattern */
     public static final String SEMICOLON = "\\s*;\\s*";
+    /** Identifier pattern */
     public static final String TRUE = "true";
+    /** Identifier pattern */
     public static final String FALSE = "false";
+    /** Identifier pattern */
     public static final String LEGAL_TYPES = String.join(
             "|", Arrays.stream(Types.values()).map(Types::toString).toList()
     );
--- src/ex5/validators/Variable.java
+++ src/ex5/validators/Variable.java
@@ -9,6 +9,14 @@ public class Variable {
     private final boolean isFinal;
     private boolean isInitialized;
 
+    /**
+     * Creates a new variable
+     *
+     * @param name         the variable's name
+     * @param type         the variable's type
+     * @param isFinal      if the variable is final
+     * @param isInitialized if the variable is initialized
+     */
     public Variable(String name, Types type, boolean isFinal, boolean isInitialized) {
         this.name = name;
         this.type = type;
@@ -16,22 +24,39 @@ public class Variable {
         this.isInitialized = isInitialized;
     }
 
+    /**
+     * @return the variable's name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * @return the variable's type
+     */
     public Types getType() {
         return type;
     }
 
+    /**
+     * @return if the variable is final
+     */
     public boolean isFinal() {
         return isFinal;
     }
 
+    /**
+     * @return if the variable is initialized
+     */
     public boolean isInitialized() {
         return isInitialized;
     }
 
+    /**
+     * Sets the variable's initialization status
+     *
+     * @param initialized the new initialization status
+     */
     public void setInitialized(boolean initialized) {
         isInitialized = initialized;
     }
